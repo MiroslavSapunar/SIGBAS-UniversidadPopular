ace25b5d552b9e4cd9335be670be72b54e585a37
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2023-09-15 01:50:04 -0300

    Update page.js
    Front del form mas completo

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -2,85 +2,242 @@
 
 export default function Home() {
   return (
-<form>
-  <div class="space-y-12">
+    <body>
+      <div class="h-full w-full fixed top-0 background" ></div>
 
-    <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="sm:col-span-3">
-          <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
-          <div class="mt-2">
-            <input 
-            type="text" 
-            name="first-name" 
-            id="first-name" 
-            autocomplete="given-name" 
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-          </div>
+    <form id ="registrationForm">
+      <div class ="formGroup flex flex-col my-4">
+        <label for="first-name">Nombre</label>
+        <div class="flex items-center">
+        <input 
+                required
+                minlength="2" 
+                type="text" 
+                name="first-name" 
+                id="first-name" 
+                autocomplete="given-name" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
         </div>
-
-        <div class="sm:col-span-3">
-          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Apellido</label>
-          <div class="mt-2">
-            <input type="text" 
-            name="last-name" 
-            id="last-name" 
-            autocomplete="family-name" 
-            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-          </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="last-name">Apellido</label>
+        <div class="flex items-center">
+        <input 
+                type="text" 
+                name="last-name" 
+                id="last-name" 
+                autocomplete="family-name" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
         </div>
-
-        <div class="sm:col-span-3">
-          <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Provincia</label>
-          <div class="mt-2">
-            <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-              <option>United States</option>
-              <option>Canada</option>
-              <option>Mexico</option>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="dni">DNI</label>
+        <div class="flex items-center">
+        <input 
+                type="number" 
+                name="dni" 
+                id="dni" 
+                autocomplete="dni" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="birthday">Fecha de nacimiento</label>
+        <div class="flex items-center">
+        <input 
+                type="date" 
+                name="birthday" 
+                id="birthday" 
+                autocomplete="birthday" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="sex" >Sexo</label>
+        <div class="mt-2">
+        <select 
+        id="sex" 
+        name="sex" 
+        autocomplete="sex" 
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+              <option>Masculino</option>
+              <option>Femenino</option>
             </select>
-          </div>
-        </div>
-
-        <div class="col-span-full">
-          <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Distrito</label>
-          <div class="mt-2">
-            <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-          </div>
-        </div>
-
-        <div class="sm:col-span-2 sm:col-start-1">
-          <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Comedor</label>
-          <div class="mt-2">
-            <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-          </div>
-        </div>
-
-        <div class="sm:col-span-2">
-          <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Fecha relevamiento </label>
-          <div class="mt-2">
-            <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-          </div>
-        </div>
-
-        <div class="sm:col-span-2">
-          <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">Numero de planilla</label>
-          <div class="mt-2">
-            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-          </div>
         </div>
       </div>
-    </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="weight">Peso</label>
+        <div class="flex items-center">
+        <input 
+                type="number" 
+                name="weight" 
+                id="weight" 
+                autocomplete="weight" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="height">Talla</label>
+        <div class="flex items-center">
+        <input 
+                type="number" 
+                name="height" 
+                id="height" 
+                autocomplete="height" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="survey-day">Fecha de relevamiento</label>
+        <div class="flex items-center">
+        <input 
+                type="date" 
+                name="survey-day" 
+                id="survey-day" 
+                autocomplete="survey-day" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="province">Provincia</label>
+        <div class="flex items-center">
+        <input 
+                type="text" 
+                name="province" 
+                id="province" 
+                autocomplete="province" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="district">Distrito</label>
+        <div class="flex items-center">
+        <input 
+                type="text" 
+                name="district" 
+                id="district" 
+                autocomplete="district" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="dining-hall">Comedor/Merendero</label>
+        <div class="flex items-center">
+        <input 
+                type="text" 
+                name="dining-hall" 
+                id="dining-hall" 
+                autocomplete="dining-hall" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <div class ="formGroup flex flex-col my-4">
+        <label for="form-number">Número de planilla</label>
+        <div class="flex items-center">
+        <input 
+                type="number" 
+                name="form-number" 
+                id="form-numbe" 
+                autocomplete="form-numbe" 
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <span class="error icon hidden-ml-6 text-red-700">
+            <i class="fa-solid fa-exclamation"></i>
+          </span>
+          <span class="success icon hidden -ml-6 text-green-700">
+            <i class="fa-solid fa-check"></i>
+          </span>
+        </div>
+          <div class="error text-red-700 py-2"></div>
+      </div>
+      <button 
+      type="submit" 
+      class="px-8 py-2 mt-4 bg-green 400 w-full rounded-full  text-white shadow-lg">
+        Registrar
+      </button>
 
-  </div>
-
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-  </div>
-</form>
+    </form> 
+    </body>
     
   )
 }
