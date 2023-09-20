3eec22d6692d7d5e84b58b3f3bb09b51ad14ec0f
XxHenry1 <henry77y@gmail.com>
2023-09-20 16:38:44 -0300

    1ra prueba login page

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'violet-university': '#4D37B7',
+    },
   },
   plugins: [],
 }
